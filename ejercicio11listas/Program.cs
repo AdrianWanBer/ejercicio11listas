1efdac9a7fe6dc59da3d4695f08161561d063751
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-04-22 13:46:46 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio11listas/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ejercicio11listas
+{
+    class program
+    {
+        public static void Main(String[] args) {
+
+            string parentesis = "((()))";
+            char parentesisAbrir = '(';
+            char parentesisCerrar = ')';
+            int contadorAbiertos = 0;
+            int contadorCerradas = 0;
+
+            foreach (char letra in parentesis) {
+
+                if (letra == parentesisAbrir)
+                {
+                    contadorAbiertos++;
+                }
+                if (letra == parentesisCerrar)
+                {
+                    contadorCerradas++;
+                }
+            }
+
+            if(contadorCerradas == contadorAbiertos)
+            {
+                Console.WriteLine("Esta balanceado");
+
+            } else
+            {
+                Console.WriteLine("No esta balanceado");
+            }
+
+        }
+    }
+}
